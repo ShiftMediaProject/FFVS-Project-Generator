cbce873a147f8b41edc451c5bee99bacb3ed23ea
Matt Oliver <protogonoi@gmail.com>
2016-07-24 23:35:05 +1000

    Add additional search paths when looking for FFmpeg dir.
    These search paths are what is common when using a downloaded release
    instead of a git clone.

--- source/configGenerator.cpp
+++ source/configGenerator.cpp
@@ -64,7 +64,7 @@ bool ConfigGenerator::passConfigureFile()
     //Setup initial directories
     if (m_sRootDirectory.length() == 0) {
         //Search paths starting in current directory then checking parents
-        string sPathList[] = {"./", "../", "../../ffmpeg/", "../../libav/", "../../../", "../../"};
+        string sPathList[] = {"./", "../", "./ffmpeg", "../ffmpeg", "../../ffmpeg/", "../../libav/", "../../../", "../../"};
         uint uiPathCount = 0;
         uint uiNumPaths = sizeof(sPathList) / sizeof(string);
         for (uiPathCount; uiPathCount < uiNumPaths; uiPathCount++) {
