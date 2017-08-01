f0e41321fb90df003a549379313af4814be07542
Matt Oliver <protogonoi@gmail.com>
2017-08-01 14:47:05 +1000

    Fix detection of DCE definitions outside DCE blocks.

--- source/projectGenerator_dce.cpp
+++ source/projectGenerator_dce.cpp
@@ -799,42 +799,46 @@ void ProjectGenerator::outputProjectDCEFindFunctions(const string & sFile, const
                 uiFindPos4 = (uiFindPos4 == string::npos) ? 0 : uiFindPos4 + 1;
                 if (uiFindPos4 == uiFindPos) {
                     uiFindPos4 = sFile.find_first_not_of(sWhiteSpace, uiFindPos3);
-                    //Check if valid function
-                    if (sFile.at(uiFindPos4) == '(') {
-                        //Check if function call or declaration (a function call must be inside a function {})
-                        uint uiCheck1 = sFile.rfind('{', uiFindPos);
-                        if (uiCheck1 != string::npos) {
-                            uint uiCheck2 = sFile.rfind('}', uiFindPos);
-                            if ((uiCheck2 == string::npos) || (uiCheck1 > uiCheck2)) {
+                    //Check if declared inside a preprocessor block
+                    uint uiFindPos5 = sFile.find('#', uiFindPos4 + 1);
+                    if ((uiFindPos5 == string::npos) || (sFile.at(uiFindPos5 + 1) != 'e')) {
+                        //Check if valid function
+                        if (sFile.at(uiFindPos4) == '(') {
+                            //Check if function call or declaration (a function call must be inside a function {})
+                            uint uiCheck1 = sFile.rfind('{', uiFindPos);
+                            if (uiCheck1 != string::npos) {
+                                uint uiCheck2 = sFile.rfind('}', uiFindPos);
+                                if ((uiCheck2 == string::npos) || (uiCheck1 > uiCheck2)) {
+                                    bValid = true;
+                                }
+                            }
+                            //Check if function definition
+                            uiCheck1 = sFile.find(')', uiFindPos4 + 1);
+                            //Skip any '(' found within the function parameters itself
+                            uint uiCheck2 = sFile.find('(', uiFindPos4 + 1);
+                            while ((uiCheck2 != string::npos) && (uiCheck2 < uiCheck1)) {
+                                uiCheck2 = sFile.find('(', uiCheck2 + 1);
+                                uiCheck1 = sFile.find(')', uiCheck1 + 1) + 1;
+                            }
+                            uiCheck2 = sFile.find_first_not_of(sWhiteSpace, uiCheck1 + 1);
+                            if (sFile.at(uiCheck2) == '{') {
                                 bValid = true;
                             }
-                        }
-                        //Check if function definition
-                        uiCheck1 = sFile.find(')', uiFindPos4 + 1);
-                        //Skip any '(' found within the function parameters itself
-                        uint uiCheck2 = sFile.find('(', uiFindPos4 + 1);
-                        while ((uiCheck2 != string::npos) && (uiCheck2 < uiCheck1)) {
-                            uiCheck2 = sFile.find('(', uiCheck2 + 1);
-                            uiCheck1 = sFile.find(')', uiCheck1 + 1) + 1;
-                        }
-                        uiCheck2 = sFile.find_first_not_of(sWhiteSpace, uiCheck1 + 1);
-                        if (sFile.at(uiCheck2) == '{') {
+                        } else if (sFile.at(uiFindPos4) == ';') {
+                            uiFindPos4 = sFile.find_last_not_of(sWhiteSpace, uiFindPos4 - 1);
+                            if (sFile.at(uiFindPos4) == '=') {
+                                bValid = true;
+                            }
+                        } else if (sFile.at(uiFindPos4) == '[') {
+                            //Check if function is a table declaration
+                            uiFindPos4 = sFile.find(']', uiFindPos4 + 1);
+                            uiFindPos4 = sFile.find_first_not_of(sWhiteSpace, uiFindPos4 + 1);
+                            if (sFile.at(uiFindPos4) == '=') {
+                                bValid = true;
+                            }
+                        } else if (sFile.at(uiFindPos4) == '=') {
                             bValid = true;
                         }
-                    } else if (sFile.at(uiFindPos4) == ';') {
-                        uiFindPos4 = sFile.find_last_not_of(sWhiteSpace, uiFindPos4 - 1);
-                        if (sFile.at(uiFindPos4) == '=') {
-                            bValid = true;
-                        }
-                    } else if (sFile.at(uiFindPos4) == '[') {
-                        //Check if function is a table declaration
-                        uiFindPos4 = sFile.find(']', uiFindPos4 + 1);
-                        uiFindPos4 = sFile.find_first_not_of(sWhiteSpace, uiFindPos4 + 1);
-                        if (sFile.at(uiFindPos4) == '=') {
-                            bValid = true;
-                        }
-                    } else if (sFile.at(uiFindPos4) == '=') {
-                        bValid = true;
                     }
                 }
             }
