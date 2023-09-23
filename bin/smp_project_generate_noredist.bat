719f556f0cbbfc12268ec3f8d0e8c3c269bfcfbf
Matt Oliver <protogonoi@gmail.com>
2023-09-23 18:33:18 +1000

    Add harfbuzz to default builds.

--- bin/smp_project_generate_noredist.bat
+++ bin/smp_project_generate_noredist.bat
@@ -9,6 +9,7 @@ freetype2, ^
 fribidi, ^
 game-music-emu, ^
 gnutls, ^
+harfbuzz, ^
 lame, ^
 libass, ^
 libbluray, ^
@@ -34,7 +35,7 @@ x265, ^
 xvid, ^
 zlib ^
 )
-SET PGOPTIONS=--enable-gpl --enable-version3 --enable-nonfree --enable-bzlib --enable-iconv --enable-lzma --enable-sdl2 --enable-zlib --enable-libmp3lame --enable-libvorbis --enable-libspeex --enable-libopus --enable-libilbc --enable-libtheora --enable-libx264 --enable-libx265 --enable-libxvid --enable-libvpx --enable-libgme --enable-libmodplug --enable-libsoxr --enable-libfreetype --enable-fontconfig --enable-libfribidi --enable-libass --enable-libxml2 --enable-gnutls --disable-schannel --enable-gcrypt --enable-libssh --enable-libcdio --enable-libbluray --enable-opengl --enable-libmfx --enable-ffnvcodec --enable-cuda --enable-amf --enable-libfdk-aac
+SET PGOPTIONS=--enable-gpl --enable-version3 --enable-nonfree --enable-bzlib --enable-iconv --enable-lzma --enable-sdl2 --enable-zlib --enable-libmp3lame --enable-libvorbis --enable-libspeex --enable-libopus --enable-libilbc --enable-libtheora --enable-libx264 --enable-libx265 --enable-libxvid --enable-libvpx --enable-libgme --enable-libmodplug --enable-libsoxr --enable-libfreetype --enable-fontconfig --enable-libfribidi --enable-libharfbuzz --enable-libass --enable-libxml2 --enable-gnutls --disable-schannel --enable-gcrypt --enable-libssh --enable-libcdio --enable-libbluray --enable-opengl --enable-libmfx --enable-ffnvcodec --enable-cuda --enable-amf --enable-libfdk-aac
 
 REM Store current directory and ensure working directory is the location of current .bat
 SET CURRDIR=%CD%
